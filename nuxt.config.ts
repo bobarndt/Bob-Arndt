beed2bc9b17a622128a20072f8bebf9346243055
Bob Arndt <bobarndt@att.net>
2022-10-19 20:40:09 -0500

    Update Nuxt config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Bob Arndt | Web design and development professional in Dallas, Texas, USA',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'Bob Arndt | Web design and development professional in Dallas, Texas, USA',
+          content: 'Bob Arndt | Web design and development professional in Dallas, Texas, USA'
+        },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.svg' }
+      ]
+    },
+  },
 
+  css: [
+    '@/assets/scss/main.scss'
+  ]
 })
