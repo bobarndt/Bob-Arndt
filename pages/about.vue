869ded1b0d47182030e8d106d72216ee83bee0c8
Bob Arndt <bobarndt@att.net>
2022-10-25 23:12:03 -0500

    Add temporary content to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 style="margin-top: 10rem">About</h1>
+    <p>Built with Nuxt3/Vue3, Sass, and Vite in VSCode. Deployed on Netlify. No React. No Tailwind. No gradients. No apology.</p>
+    <p>No Facebook. No Instagram. No Twitter. No pre-natal murder. No climate change. No foreign invasion. No Fauci. No Gates. No WHO. No vaccination. No cancel. No woke. No Antifa. No Marxism. No Democrats. No Joe. No Bernie. No Beto. No UN. No FBI. No DOJ. No BLM. No CNN. No NPR. No NFL. No NBA. No MLB. No ESPN. No Amazon. No Starbucks. No Disney. No apology.</p>
+    <p>Ultra MAGA. Re-fund the police. Finish the wall. No compromise. Trump 2024. No apology.</p>
   </div>
 </template>
 
