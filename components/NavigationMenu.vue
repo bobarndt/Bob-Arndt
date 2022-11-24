2d109a52e350c21cd2526f8c79c04dd0d2e44036
Bob Arndt <bobarndt@att.net>
2022-11-24 03:31:24 -0600

    Refactor and restyle navigation menu

--- components/NavigationMenu.vue
+++ components/NavigationMenu.vue
@@ -4,10 +4,18 @@
 
       <nav class="menu-nav">
         <ul class="menu-list">
-          <li class="menu-item"><a class="menu-link" href="/"><ion-icon src="/images/home.svg"></ion-icon><span data-content="Home">Home</span></a></li>
-          <li class="menu-item"><a class="menu-link" href="/about"><span data-content="About">About</span></a></li>
-          <li class="menu-item"><a class="menu-link" href="/resume"><span data-content="Resume">Resume</span></a></li>
-          <li class="menu-item"><a class="menu-link" href="/contact"><span data-content="Contact Me">Contact Me</span></a></li>
+          <div class="menu-item-wrapper">
+            <li class="menu-item"><a class="menu-link" href="/"><ion-icon src="/images/home.svg"></ion-icon><span data-content="Home">Home</span></a></li>
+          </div>
+          <div class="menu-item-wrapper">
+            <li class="menu-item"><a class="menu-link" href="/about"><ion-icon src="/images/about-me.svg"></ion-icon><span data-content="About Me">About Me</span></a></li>
+          </div>
+          <div class="menu-item-wrapper">
+            <li class="menu-item"><a class="menu-link" href="/resume"><ion-icon src="/images/resume.svg"></ion-icon><span data-content="Resume">Resume</span></a></li>
+          </div>
+          <div class="menu-item-wrapper">
+            <li class="menu-item"><a class="menu-link" href="/contact"><ion-icon src="/images/contact-me.svg"></ion-icon><span data-content="Contact Me">Contact Me</span></a></li>
+          </div>
         </ul>
       </nav>
 
@@ -62,7 +70,7 @@
 <style lang="scss" scoped>
   .menu-content {
     background-color: var(--color-menu-background);
-    border: .5rem solid rgba(0, 0, 0, .25);
+    border: .75rem solid rgba(0, 0, 0, .25);
     display: grid;
     font-size: var(--font-size-xlarge);
     grid-template-rows: 1fr 15rem;
@@ -74,18 +82,26 @@
   }
 
   .menu-item {
-    line-height: var(--line-height-menu-item);
+    text-align: center;
 
     &.active a span {
-        color: var(--color-azure-500);
-      }
-}
+      color: var(--color-azure-400);
+    }
+  }
+
+  .menu-item-wrapper {
+    align-items: center;
+    border-radius: 1.5rem;
+    display: flex;
+    height: 4.5rem;
+    justify-content: start;
+  }
 
   .menu-link {
     cursor: pointer;
+    display: inline-block;
     font-weight: var(--font-weight-xbold);
     letter-spacing: var(--letter-spacing-xxwide);
-    position: relative;
     text-decoration: none;
     transition: clip-path 275ms ease;
 
@@ -94,30 +110,51 @@
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
     }
 
+    &:hover ion-icon {
+      animation: pulse-icon-menu 2s linear 2;
+      color: var(--color-white);
+    }
+
     span {
-      color: var(--color-slate-200);
+      color: var(--color-slate-400);
       display: inline-block;
       position: relative;
 
       &::before {
         content: attr(data-content);
         clip-path: polygon(0 0, 0 0, 0% 100%, 0 100%);
-        color: var(--color-link-overlay);
+        color: var(--color-menu-link-overlay);
         letter-spacing: var(--letter-spacing-xxwide);
         position: absolute;
         text-decoration: underline;
-        text-decoration-color: var(--color-link-overlay);
+        text-decoration-color: var(--color-menu-link-overlay);
         transition: clip-path 350ms ease;
+      }
+    }
+
+    ion-icon {
+      color: var(--color-slate-400);
+      margin-bottom: -.2rem;
+      margin-right: 4rem;
+
+      &:hover {
+        animation: pulse-icon-menu 2s linear 2;
+        color: var(--color-white);
       }
     }
   }
 
   .menu-list {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    justify-content: space-between;
     list-style: none;
+    width: 100%;
   }
 
   .menu-nav {
-    margin-top: 10rem;
+    margin: 8rem auto 0;
   }
 
   .menu-tech-stack {
