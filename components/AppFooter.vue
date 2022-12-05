e6ee33f808757c9fc64acb404d4fcb6b0cd4525c
Bob Arndt <bobarndt@att.net>
2022-12-04 19:32:01 -0600

    Update copyright dates in footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -9,7 +9,7 @@
     <div class="footer-row-wrapper-bottom">
       <div class="footer-row-bottom">
       <div class="footer-photo-wrapper"></div>
-      <span class="footer-copyright">© 2022 REAlizations, Inc. All rights reserved.</span>
+      <span class="footer-copyright">© 2022-23 REAlizations, Inc. All rights reserved.</span>
       </div>
     </div>
   </footer>
