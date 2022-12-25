166a82c140e50277bd5f89723fae71cf66b53f18
Bob Arndt <bobarndt@att.net>
2022-12-24 18:27:00 -0600

    Reworked nuxt configuration file to updated meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,8 @@ export default defineNuxtConfig({
           content: `width=device-width, initial-scale=1`
         },
         {
-          name: `Bob Arndt | Web design and development professional in Dallas, Texas, USA`,
-          content: `Bob Arndt | Web design and development professional in Dallas, Texas, USA`
+          name: `Bob Arndt`,
+          content: `bobarndt.com is the personal website of front end web developer Bob Arndt of Dallas, Texas, USA, and was developed using JavaScript, HTML5, CSS3, Nuxt3, Vue3, Sass and Vite in VSCode and deployed on Netlify.`
         },
       ],
       script: [],
